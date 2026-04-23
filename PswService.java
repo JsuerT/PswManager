e47a63bd23aef4f52b45d4544af8c2db0f2e16c1
Jojo (Ticco) <johanna.suermann@iu-study.org>
2026-04-23 11:37:44 +0200

    setup, addentry

---
+++ PswService.java
@@ -0,0 +1,73 @@
+package service;
+
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.util.Scanner;
+
+public class PswService {
+    private static class Entry {
+        private String application, username, psw, email, additionalInfo;
+        
+        public Entry(String application, String username, String psw, String email, String additionalInfo) {
+            this.application = application;
+            this.username = username;
+            this.psw = psw;
+            this.email = email;
+            this.additionalInfo = additionalInfo;
+        }
+
+        public String toFileString() {
+            return application + ";" + username + ";" + psw + ";" + email + ";" + additionalInfo;
+        }
+    }
+
+    public static void addPswEntry(Scanner scanner) {
+        File pswFile = new File("pswFile.txt");
+        
+        try {
+            if (pswFile.createNewFile()) {
+                System.out.println("File created: " + pswFile.getName());
+            }
+        } catch (IOException e) {
+            System.out.println("Error while creating pswFile");
+            e.printStackTrace();
+        }
+        
+        Entry newEntry = addInput(scanner);
+        
+        saveToFile(pswFile, newEntry);
+    }
+
+    private static Entry addInput(Scanner scanner) {
+        System.out.println("Enter application:");
+        String application = scanner.nextLine();
+
+        System.out.println("Enter username:");
+        String username = scanner.nextLine();
+
+        System.out.println("Enter psw:");
+        String psw = scanner.nextLine();
+
+        System.out.println("Enter email:");
+        String email = scanner.nextLine();
+
+        System.out.println("Enter additionalInfo:");
+        String additionalInfo = scanner.nextLine();
+
+        return new Entry(application, username, psw, email, additionalInfo);
+    }
+
+    private static void saveToFile(File file, Entry entry) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+            writer.write(entry.toFileString());
+            writer.newLine(); 
+            System.out.println("Entry successfully saved to " + file.getName());
+        } catch (IOException e) {
+            System.out.println("Error while writing to file");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Start.java
@@ -0,0 +1,38 @@
+import service.PswService;
+import java.util.Scanner;
+
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Start{
+  public static void main (String[]args){
+    //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Passwordmanagementsystem \n What do you want to do \n");
+    System.out.println(
+        "1) Write a new entry \n" +
+        "2) View an existing entry \n"+ 
+        "3) Close whole application"
+        );
+
+    int selection;
+    do{
+      System.out.println("Enter a valid number:");
+      selection = sc.nextInt();
+      sc.nextLine();
+      switch(selection){
+        case 1:
+          System.out.println("NEW ENTRY");
+          PswService.addPswEntry(sc);
+          break;
+        case 2:
+          System.out.println("VIEW ENTRIES");
+          break;
+        case 3: 
+          System.out.println("BYE");
+          System.exit(0); 
+      }
+    }while(selection > 3);
+  }
+}
